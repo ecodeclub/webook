40dca2afc5069fdef6c164dbd44a596f291cf5a0
WenZu-Zhou <140875086+WenZu-Zhou@users.noreply.github.com>
2023-09-14 23:16:49 +0800

    fix: user_test.go

--- internal/web/user_test.go
+++ internal/web/user_test.go
@@ -190,7 +190,6 @@ func TestUserHandler_SignUp(t *testing.T) {
 	}
 }
 
-
 func TestUserHandler_EmailVerify(t *testing.T) {
 	lg, err := zap.NewDevelopment()
 	if err != nil {
@@ -395,7 +394,7 @@ func TestUserHandle_TokenLogin(t *testing.T) {
 	now := time.Now()
 	testCases := []struct {
 		name        string
-		mock        func(ctl *gomock.Controller) service.UserAndService
+		mock        func(ctl *gomock.Controller) service.UserService
 		reqBody     string
 		wantCode    int
 		wantBody    string
@@ -404,7 +403,7 @@ func TestUserHandle_TokenLogin(t *testing.T) {
 	}{
 		{
 			name: "参数绑定失败",
-			mock: func(ctl *gomock.Controller) service.UserAndService {
+			mock: func(ctl *gomock.Controller) service.UserService {
 				return nil
 			},
 			reqBody:     `{"email":"asxxxxxxxxxx163.com","password":"123456","fingerprint":"for-test"}`,
@@ -414,7 +413,7 @@ func TestUserHandle_TokenLogin(t *testing.T) {
 		},
 		{
 			name: "登录成功",
-			mock: func(ctl *gomock.Controller) service.UserAndService {
+			mock: func(ctl *gomock.Controller) service.UserService {
 				return nil
 			},
 			reqBody:     `{"email":"asxxxxxxxxxx@163.com","password":"123456","fingerprint":"for-test"}`,
@@ -429,14 +428,14 @@ func TestUserHandle_TokenLogin(t *testing.T) {
 			ctrl := gomock.NewController(t)
 			defer ctrl.Finish()
 			server := gin.New()
-			h := NewUserHandler(tc.mock(ctrl))
+			h := NewUserHandler(tc.mock(ctrl), nil, nil, nil, "", nil)
 			h.RegisterRoutes(server)
 
 			req, err := http.NewRequest(http.MethodPost, "/users/login", bytes.NewBuffer([]byte(tc.reqBody)))
 			require.NoError(t, err)
 			req.Header.Set("Content-Type", "application/json")
 
-			//用于接收resp
+			// 用于接收resp
 			resp := httptest.NewRecorder()
 
 			server.ServeHTTP(resp, req)
@@ -444,7 +443,7 @@ func TestUserHandle_TokenLogin(t *testing.T) {
 			assert.Equal(t, tc.wantCode, resp.Code)
 
 			assert.Equal(t, tc.wantBody, resp.Body.String())
-			//登录成功才需要判断
+			// 登录成功才需要判断
 			if resp.Code == http.StatusOK {
 				accessToken := resp.Header().Get("x-access-token")
 				refreshToken := resp.Header().Get("x-refresh-token")
@@ -460,7 +459,7 @@ func TestUserHandle_TokenLogin(t *testing.T) {
 					panic("强制类型转换失败")
 				}
 				assert.Equal(t, tc.fingerprint, accessTokenClaim.Fingerprint)
-				//判断过期时间
+				// 判断过期时间
 				if now.Add(time.Minute*29).UnixMilli() > accessTokenClaim.RegisteredClaims.ExpiresAt.Time.UnixMilli() {
 					panic("过期时间异常")
 				}
@@ -474,14 +473,15 @@ func TestUserHandle_TokenLogin(t *testing.T) {
 				}
 				refreshTokenClaim := refreshT.(*TokenClaims)
 				assert.Equal(t, tc.fingerprint, refreshTokenClaim.Fingerprint)
-				//判断过期时间
+				// 判断过期时间
 				if now.Add(time.Hour*168).UnixMilli() < accessTokenClaim.RegisteredClaims.ExpiresAt.Time.UnixMilli() {
 					panic("过期时间异常")
 				}
 			}
-  )}
+		})
+	}
 }
-  
+
 func CreateToken() (string, string) {
 	now := time.Now()
 	claims := TokenClaims{
@@ -495,7 +495,7 @@ func CreateToken() (string, string) {
 
 	claims.RegisteredClaims.ExpiresAt = jwt.NewNumericDate(now.Add(time.Hour * 168))
 	token = jwt.NewWithClaims(jwt.SigningMethodHS512, claims)
-	//下面的密钥可以使用不同的密钥(一样的也行)
+	// 下面的密钥可以使用不同的密钥(一样的也行)
 	refreshToken, _ := token.SignedString([]byte(RefreshSecret))
 	return tokenStr, refreshToken
 }
@@ -509,15 +509,15 @@ func Decrypt(encryptString string, secret string) (interface{}, error) {
 		fmt.Println("解析失败:", err)
 		return nil, err
 	}
-	//检查过期时间
+	// 检查过期时间
 	if claims.ExpiresAt.Time.Before(time.Now()) {
-		//过期了
+		// 过期了
 
 		return nil, err
 	}
-	//TODO 这里测试按需判断 claims.Uid
+	// TODO 这里测试按需判断 claims.Uid
 	if token == nil || !token.Valid {
-		//解析成功  但是 token 以及 claims 不一定合法
+		// 解析成功  但是 token 以及 claims 不一定合法
 
 		return nil, err
 	}
