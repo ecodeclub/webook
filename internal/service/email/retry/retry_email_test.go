b477b3a9d275cfb98818337271324f48ee883057
chenmingyong <chenmingyong5873@126.com>
2023-09-22 15:00:08 +0800

    修复 mocks 目录变更，导致 retry_email_test.go 文件 import 报错的问题

--- internal/service/email/retry/retry_email_test.go
+++ internal/service/email/retry/retry_email_test.go
@@ -12,7 +12,7 @@ import (
 	"github.com/ecodeclub/ekit/retry"
 
 	"github.com/ecodeclub/webook/internal/service/email"
-	evcmocks "github.com/ecodeclub/webook/internal/service/email/mocks"
+	evcmocks "github.com/ecodeclub/webook/internal/service/mocks"
 )
 
 func TestService_Send(t *testing.T) {
