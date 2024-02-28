0069b2e833790b7ea75b4bbec136828b2c372363
longyue0521 <longyueli0521@gmail.com>
2024-02-28 13:20:20 +0800

    重构ci
    Signed-off-by: longyue0521 <longyueli0521@gmail.com>

--- ioc/db.go
+++ ioc/db.go
@@ -15,9 +15,44 @@
 package ioc
 
 import (
+	"context"
+	"database/sql"
+	"time"
+
+	"github.com/ecodeclub/ekit/retry"
 	"github.com/ego-component/egorm"
+	"github.com/gotomicro/ego/core/econf"
 )
 
 func InitDB() *egorm.Component {
+	WaitForDBSetup(econf.GetString("mysql.dsn"))
 	return egorm.Load("mysql").Build()
+}
+
+func WaitForDBSetup(dsn string) {
+	sqlDB, err := sql.Open("mysql", dsn)
+	if err != nil {
+		panic(err)
+	}
+	const maxInterval = 10 * time.Second
+	const maxRetries = 10
+	strategy, err := retry.NewExponentialBackoffRetryStrategy(time.Second, maxInterval, maxRetries)
+	if err != nil {
+		panic(err)
+	}
+
+	const timeout = 5 * time.Second
+	for {
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		err = sqlDB.PingContext(ctx)
+		cancel()
+		if err == nil {
+			break
+		}
+		next, ok := strategy.Next()
+		if !ok {
+			panic("WaitForDBSetup 重试失败......")
+		}
+		time.Sleep(next)
+	}
 }
