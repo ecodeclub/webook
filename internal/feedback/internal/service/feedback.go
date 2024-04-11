8fadf906c0529ce185770a618a8aa97fd15cee8f
Longyue Li <longyueli0521@gmail.com>
2024-04-11 20:27:03 +0800

    重构feedback模块并添加发送增加积分的消息代码及测试代码 (#85)
    Signed-off-by: longyue0521 <longyueli0521@gmail.com>

--- internal/feedback/internal/service/feedback.go
+++ internal/feedback/internal/service/feedback.go
@@ -1,32 +1,55 @@
+// Copyright 2023 ecodeclub
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package service
 
 import (
 	"context"
-
-	"github.com/gotomicro/ego/core/elog"
+	"fmt"
 
 	"github.com/ecodeclub/webook/internal/feedback/internal/domain"
+	"github.com/ecodeclub/webook/internal/feedback/internal/event"
 	"github.com/ecodeclub/webook/internal/feedback/internal/repository"
+	"github.com/gotomicro/ego/core/elog"
+	"github.com/lithammer/shortuuid/v4"
 )
 
 type Service interface {
-	// 管理端
-	// 列表 根据交互来
+	// List 管理端 列表 根据交互来
 	List(ctx context.Context, offset, limit int) ([]domain.Feedback, error)
+	// PendingCount 未处理的数量
 	PendingCount(ctx context.Context) (int64, error)
-	// 详情
+	// Info 详情
 	Info(ctx context.Context, id int64) (domain.Feedback, error)
-	// 处理 反馈
+	// UpdateStatus 处理 反馈
 	UpdateStatus(ctx context.Context, domainFeedback domain.Feedback) error
-	//	c端
-	// 添加
+	// Create c端 添加
 	Create(ctx context.Context, feedback domain.Feedback) error
 }
 
 type service struct {
-	repo repository.FeedBackRepo
-	//creditsEventProducer *event.CreditsEventProducer
-	logger *elog.Component
+	repo     repository.FeedbackRepository
+	producer event.IncreaseCreditsEventProducer
+	logger   *elog.Component
+}
+
+func NewFeedbackService(repo repository.FeedbackRepository, producer event.IncreaseCreditsEventProducer) Service {
+	return &service{
+		repo:     repo,
+		logger:   elog.DefaultLogger,
+		producer: producer,
+	}
 }
 
 func (s *service) PendingCount(ctx context.Context) (int64, error) {
@@ -37,25 +60,31 @@ func (s *service) Info(ctx context.Context, id int64) (domain.Feedback, error) {
 	return s.repo.Info(ctx, id)
 }
 
-func (s *service) UpdateStatus(ctx context.Context, domainFeedback domain.Feedback) error {
-	err := s.repo.UpdateStatus(ctx, domainFeedback.ID, domainFeedback.Status)
+func (s *service) UpdateStatus(ctx context.Context, feedback domain.Feedback) error {
+	info, err := s.repo.Info(ctx, feedback.ID)
+	if err != nil {
+		return fmt.Errorf("反馈ID非法: %w", err)
+	}
+	err = s.repo.UpdateStatus(ctx, feedback.ID, feedback.Status)
 	if err != nil {
 		return err
 	}
-	// todo 添加发送反馈成功时间
-	//if domainFeedback.Status == domain.Access {
-
-	//evt := event.CreditsEvent{
-	//	Uid: uid,
-	//}
-	//if eerr := s.creditsEventProducer.Produce(ctx, evt); eerr != nil {
-	//	s.logger.Error("发送反馈成功消息失败",
-	//		elog.FieldErr(eerr),
-	//		elog.FieldKey("event"),
-	//		elog.FieldValueAny(evt),
-	//	)
-	//}
-	//}
+	if feedback.Status == domain.Adopt {
+		evt := event.CreditIncreaseEvent{
+			Key:    shortuuid.New(),
+			Uid:    info.UID,
+			Amount: 100,
+			Biz:    9,
+			BizId:  info.ID,
+			Action: "采纳反馈",
+		}
+		if er := s.producer.Produce(ctx, evt); er != nil {
+			s.logger.Error("发送增加积分消息失败",
+				elog.FieldErr(er),
+				elog.Any("event", evt),
+			)
+		}
+	}
 	return nil
 }
 
@@ -65,11 +94,4 @@ func (s *service) Create(ctx context.Context, feedback domain.Feedback) error {
 
 func (s *service) List(ctx context.Context, offset, limit int) ([]domain.Feedback, error) {
 	return s.repo.List(ctx, offset, limit)
-}
-
-func NewService(repo repository.FeedBackRepo) Service {
-	return &service{
-		repo:   repo,
-		logger: elog.DefaultLogger,
-	}
 }
