69abfd963a0127e61d7b61a39a9c9618b062bfe1
Deng Ming <ming_flycash@qq.com>
2024-08-17 21:20:24 +0800

    工作经历 API

---
+++ internal/resume/internal/web/experience.go
@@ -0,0 +1,62 @@
+// Copyright 2023 ecodeclub
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package web
+
+import (
+	"github.com/ecodeclub/ginx"
+	"github.com/ecodeclub/ginx/session"
+	"github.com/gin-gonic/gin"
+)
+
+type ExperienceHandler struct {
+}
+
+func (h *ExperienceHandler) PrivateRoutes(server *gin.Engine) {
+	g := server.Group("/resume/experience")
+	g.POST("/save", ginx.BS[Experience](h.Save))
+	g.POST("/list", ginx.S(h.List))
+	g.POST("/delete", ginx.S(h.Delete))
+}
+
+// Save 创建或者更新，如果 req 里面有 id 就是更新
+func (h *ExperienceHandler) Save(ctx *ginx.Context,
+	req Experience,
+	sess session.Session) (ginx.Result, error) {
+	panic("implement me")
+}
+
+// List 查询某个人的项目经历
+func (h *ExperienceHandler) List(ctx *ginx.Context, sess session.Session) (ginx.Result, error) {
+	// 可以尝试检测 gap，提醒他你需要一个理由
+	panic("implement")
+}
+
+// Delete 删除某段项目经历
+func (h *ExperienceHandler) Delete(ctx *ginx.Context, sess session.Session) (ginx.Result, error) {
+	panic("implement me")
+}
+
+// MatchJob 这段工作经历和一个 JD 的匹配程度
+// 暂时不需要实现
+// 输入 JD，输入一个，要求 AI 输出匹配程度， prompt 里面要提示从哪些角度去匹配
+func (h *ExperienceHandler) MatchJob(ctx *ginx.Context, sess session.Session) (ginx.Result, error) {
+	panic("implement me")
+}
+
+// AIAnalysis AI 对这份工作经历的分析，也就是写得好不好，是否有竞争力
+// Experience 输入，prompt 要从哪些角度分析这段工作经历，
+func (h *ExperienceHandler) AIAnalysis(ctx *ginx.Context, sess session.Session) (ginx.Result, error) {
+	panic("implement me")
+}
