b88126f73fddfa3b030785e049f8b8747e468ff4
zwl <1633720889@qq.com>
2025-10-31 15:27:43 +0800

    调整Roadmap结构

--- ioc/gin.go
+++ ioc/gin.go
@@ -16,7 +16,9 @@ package ioc
 
 import (
 	"net/http"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/ecodeclub/webook/internal/company"
 
@@ -130,6 +132,15 @@ func initGinxServer(sp session.Provider,
 	// 微信支付的回调不需要安全校验机制
 	paymentHdl.PublicRoutes(res.Engine)
 
+	res.Use(func(ctx *gin.Context) {
+		ctx.Set("_session", session.NewMemorySession(session.Claims{
+			Uid: 1,
+			Data: map[string]string{
+				"creator":   "true",
+				"memberDDL": strconv.FormatInt(time.Now().Add(time.Hour).UnixMilli(), 10),
+			},
+		}))
+	})
 	// 虽然叫做 NonSense，但是我还是得告诉你，这是一个安全校验机制
 	// 但是我并不能在开源里面放出来，因为知道了如何校验，就知道了如何破解
 	// 虽然理论上可以用 plugin 机制，但是 plugin 机制比较容易遇到不兼容的问题
